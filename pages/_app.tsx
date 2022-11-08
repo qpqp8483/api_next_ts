504c2fdd1332040fc6acc5a71b585687cd6d9d3a
chanwoo <wkdcksdn1996@gmail.com>
2022-11-08 23:48:12 +0900

    header 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
 import type { AppProps } from "next/app";
 import Top from "./src/component/Top";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Top />
-      <Component {...pageProps} />
+      <div style={{ width: 1000, margin: "0 auto" }}>
+        <Top />
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
