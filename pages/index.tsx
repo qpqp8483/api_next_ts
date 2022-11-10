78d4085784ba367fa11f7690e64f84ebfe9fffb2
chanwoo <wkdcksdn1996@gmail.com>
2022-11-11 01:29:22 +0900

    index 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { Divider, Header } from "semantic-ui-react";
 import axios from "axios";
-const Home: NextPage = ({ list }) => {
+import ItemList from "../src/component/ItemList";
+import { ProductItemList } from "../src/models/product";
+
+interface Props {
+  list: ProductItemList;
+}
+
+const Home: NextPage<Props> = ({ list }) => {
   return (
     <div>
       <Head>
@@ -10,9 +17,10 @@ const Home: NextPage = ({ list }) => {
         <meta name="description" content="차누땅땅 홈입니다." />
       </Head>
       <Header style={{ paddingTop: 40 }}>베스트 상품</Header>
-      {list}
+      <ItemList list={list.slice(0, 9)} />
       <Divider />
       <Header style={{ paddingTop: 40 }}>신상품</Header>
+      <ItemList list={list.slice(9)} />
       <Divider />
     </div>
   );
@@ -20,20 +28,14 @@ const Home: NextPage = ({ list }) => {
 
 export default Home;
 
-interface IList {
-  props: {
-    list: string;
-  };
-}
-
-export async function getStaticProps(): Promise<IList> {
-  const API_URL = process.env.apiurl;
-  const res = await axios.get(API_URL as string);
-  const data: string = res.data;
+export const getStaticProps: GetStaticProps = async () => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+  const res = await axios.get(API_URL);
+  const data: ProductItemList = res.data;
 
   return {
     props: {
       list: data,
     },
   };
-}
+};
