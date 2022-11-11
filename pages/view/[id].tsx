57029c00a72e6824ffe1c01f309192c079996351
chanwoo <qpqp8483@naver.com>
2022-11-11 17:49:47 +0900

    오류 수정 작업

--- pages/view/[id].tsx
+++ pages/view/[id].tsx
@@ -1,15 +1,31 @@
-import { GetServerSideProps, GetStaticProps } from "next";
+import axios from "axios";
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect } from "react";
+import Item from "../../src/component/Item";
+import { ProductItemList, ProductItem } from "../../src/models/product";
 
-const Detail = ({ context }) => {};
+interface Props {
+  item: ProductItem;
+}
 
-export default Detail;
+const Post: NextPage<Props> = ({ item }) => {
+  return (
+    <div>
+      <Item item={item}></Item>
+    </div>
+  );
+};
+
+export default Post;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  //   const id = context.params.id;
-  //   const API_URL;
+  const id = context.query.id as string; // 질문 : params로 하면 오류 -> ParsedUrlQuery | undefined로 인식 ->타입을 어떻게 설정해야하는지 모르겠음
+  const API_URL: string = `http:makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(API_URL);
+  const data: ProductItemList = res.data;
   return {
     props: {
-      context,
+      item: data,
     },
   };
 };
